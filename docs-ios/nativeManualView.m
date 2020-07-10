0e002b9df948e70d78951b89f5cbebfcf54768ca
James Morris <47285129+jamesmorrisapps@users.noreply.github.com>
2020-07-09 20:25:57 -0700

    Initial Commit

---
+++ docs-ios/nativeManualView.m
@@ -0,0 +1,16 @@
+// NativeAdCustomView.m
+#import "NativeAdCustomView.h"
+@import SmaatoSDKCore;
+@import SmaatoSDKNative;
+
+// make a native ad custom view that is of type UIView
+@interface NativeAdCustomView()
+
+@property (weak, nonatomic) UILabel *titleTextLabel;
+@property (weak, nonatomic) UILabel *mainTextLabel;
+@property (weak, nonatomic) UILabel *ctaLabel;
+@property (weak, nonatomic) UILabel *sponsoredLabel;
+@property (weak, nonatomic) UIImageView *mainImageView;
+@property (weak, nonatomic) UIImageView *iconImageView;
+
+@end
