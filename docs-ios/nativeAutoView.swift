f0d2b7f81a87cda1e39a7cc2538b82783353aaf1
James Morris <47285129+jamesmorrisapps@users.noreply.github.com>
2020-07-10 16:52:10 -0700

    Initial Commit

---
+++ docs-ios/nativeAutoView.swift
@@ -0,0 +1,13 @@
+import UIKit
+import SmaatoSDKCore
+import SmaatoSDKNative
+ 
+class NativeAdSmaatoView: UIView, SMANativeView {
+    @IBOutlet weak var smaSponsoredLabel: UILabel!
+    @IBOutlet weak var smaMainTextLabel: UILabel!
+    @IBOutlet weak var smaTitleLabel: UILabel!
+    @IBOutlet weak var smaIconView: UIView!
+    @IBOutlet weak var smaMediaView: UIView!
+    @IBOutlet weak var smaCTALabel: UILabel!
+    @IBOutlet weak var smaPrivacyView: UIView!
+}
