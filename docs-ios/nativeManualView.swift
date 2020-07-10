5d6ffc713eb6f79b3eb1fe1493f8ea39aa55d4ff
James Morris <47285129+jamesmorrisapps@users.noreply.github.com>
2020-07-10 16:50:54 -0700

    Initial Commit

---
+++ docs-ios/nativeManualView.swift
@@ -0,0 +1,12 @@
+import UIKit
+import SmaatoSDKCore
+import SmaatoSDKNative
+ 
+class NativeAdCustomView: UIView {
+    @IBOutlet weak var titleTextLabel: UILabel!
+    @IBOutlet weak var mainTextLabel: UILabel!
+    @IBOutlet weak var ctaLabel: UILabel!
+    @IBOutlet weak var sponsoredLabel: UILabel!
+    @IBOutlet weak var mainImageView: UIImageView!
+    @IBOutlet weak var iconImageView: UIImageView!
+}
