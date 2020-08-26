6ef0dd60f11a89eb9caefa9b9caba9e1f2fc8725
James Morris <47285129+jamesmorrisapps@users.noreply.github.com>
2020-08-25 18:08:56 -0700

    Initial Commit

---
+++ docs-ios/prebidInterstitial.m
@@ -0,0 +1,34 @@
+MPInterstitialAdController *adInterstitial = [MPInterstitialAdController interstitialAdControllerForAdUnitId:@"<MOPUB_ADUNIT_ID>"];
+adInterstitial.delegate = self;
+[SmaatoSDK prebidInterstitialForAdSpaceId:@"<SMAATO_ADSPACE_ID>"
+                               completion:^(SMAUbBid * _Nullable bid, NSError * _Nullable error) {
+    if (bid) {
+        // Let's assume this is the max price
+        CGFloat maxPrice = 0.1;
+        NSString *bidKeyword;
+                                            
+        if (bid.bidPrice > maxPrice) {
+            bidKeyword = [NSString stringWithFormat:@"smaato_cpm:%.2f", maxPrice];
+        } else {
+            bidKeyword = bid.mopubPrebidKeyword;
+        }
+        NSString *adViewKeywords = adInterstitial.keywords;
+        if (adViewKeywords.length > 0) {
+            NSPredicate *predicate = [NSPredicate predicateWithFormat:@"NOT (SELF beginswith[c] 'smaato_cpm')"];
+            NSMutableArray *filteredKeywords = [[[adViewKeywords componentsSeparatedByString:@","] filteredArrayUsingPredicate:predicate] mutableCopy];
+            [filteredKeywords addObject:bidKeyword];
+            adInterstitial.keywords = [filteredKeywords componentsJoinedByString:@","];
+        } else {
+            adInterstitial.keywords = bidKeyword;
+        }
+ 
+        if (adInterstitial.localExtras) {
+            NSMutableDictionary *extras = [NSMutableDictionary dictionaryWithDictionary:adInterstitial.localExtras];
+            [extras addEntriesFromDictionary:bid.metaData];
+            adInterstitial.localExtras = extras;
+        } else {
+            adInterstitial.localExtras = bid.metaData;
+        }
+    }
+    [adInterstitial loadAd];
+}];
