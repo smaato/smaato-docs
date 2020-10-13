ba8610b76e086639d7d0188f919c417500b5b02d
James Morris <47285129+jamesmorrisapps@users.noreply.github.com>
2020-10-13 13:29:05 -0700

    Added semicolon and label

--- docs-android/ccpa.java
+++ docs-android/ccpa.java
@@ -3,7 +3,7 @@ import android.preference.PreferenceManager;
 
 ...
 // save the user's consent
-SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context)
+SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context_here);
 SharedPreferences.Editor editor = sharedPref.edit();
 editor.putString("IABUSPrivacy_String", "1YNN"); // for example "1YNN"
 editor.commit();
