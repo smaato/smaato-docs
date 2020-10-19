89b565aa264de191d9637a27f8baaa65c82cd1f7
James Morris <47285129+jamesmorrisapps@users.noreply.github.com>
2020-10-19 13:41:22 -0700

    Updated SharedPreferences Value to TCFv2

--- docs-android/gdpr.java
+++ docs-android/gdpr.java
@@ -5,6 +5,6 @@ import android.preference.PreferenceManager;
 // User is not subject to GDPR
 SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context_here);
 SharedPreferences.Editor editor = sharedPref.edit();
-editor.putString("IABConsent_SubjectToGDPR", "0");
+editor.putString("IABTCF_gdprApplies", "0");
 editor.commit();
 // Use this example above to set values for the rest of the GDPR keys
