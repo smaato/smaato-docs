88f29ebd8245e24b80e39cdaba9901c9522708f5
James Morris <47285129+jamesmorrisapps@users.noreply.github.com>
2020-12-02 19:39:36 -0800

    Removed viewDidLoad Method

--- docs-ios/setupSplashAppLaunch.swift
+++ docs-ios/setupSplashAppLaunch.swift
@@ -6,7 +6,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     private var interstitial: SMAInterstitial?
  
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-         super.viewDidLoad()
          let params = SMAAdRequestParams()
          params.isSplash = true
          SmaatoSDK.loadInterstitial(forAdSpaceId: "SMAATO_ADSPACE_ID", delegate: self, requestParams: params)
