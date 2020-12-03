6f5f04773f4fc469ad65f998feed6b0dc559908f
James Morris <47285129+jamesmorrisapps@users.noreply.github.com>
2020-12-02 17:56:02 -0800

    Initial Commit

---
+++ docs-ios/setupSplashAppLaunch.m
@@ -0,0 +1,34 @@
+// AppDelegate.m
+#import "AppDelegate.h"
+@import SmaatoSDKInterstitial;
+ 
+// declare the AppDelegate to conform the SMAInterstitialDelegate protocol
+@interface AppDelegate () <SMAInterstitialDelegate>
+@property (nonatomic, strong) SMAInterstitial *interstitial;
+@end
+ 
+@implementation AppDelegate
+
+ - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    SMAAdRequestParams *requestParams = [SMAAdRequestParams new];
+    requestParams.isSplash = YES;
+    [SmaatoSDK loadInterstitialForAdSpaceId:@"SMAATO_ADSPACE_ID" delegate:self requestParams:requestParams];
+}
+
+// Interstitial did successfully loaded
+- (void)interstitialDidLoad:(SMAInterstitial *)interstitial {
+    self.interstitial = interstitial;
+    [self.interstitial showFromViewController:self];
+}
+  
+// Interstitial did fail loading
+- (void)interstitial:(SMAInterstitial *)interstitial didFailWithError:(NSError *)error {
+    NSLog(@"Interstitial did fail loading with error: %@", error.localizedDescription);
+}
+ 
+// Interstitial ads TTL has expired
+- (void)interstitialDidTTLExpire:(SMAInterstitial *)interstitial {
+    NSLog(@"Interstitial TTL has expired");
+}
+ 
+@end
