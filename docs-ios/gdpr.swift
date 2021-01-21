a97edb52a3fd4e1f3e7f73c5cc06fd9a558fdd1e
James Morris <47285129+jamesmorrisapps@users.noreply.github.com>
2021-01-21 06:18:11 -0800

    Updated from NSString to NSNumber

--- docs-ios/gdpr.swift
+++ docs-ios/gdpr.swift
@@ -1,3 +1,3 @@
 // User is not subject to GDPR
-UserDefaults.standard.set("0", forKey: "IABTCF_gdprApplies")
+UserDefaults.standard.set(0, forKey: "IABTCF_gdprApplies")
 // Use this example above to set values for the rest of the GDPR keys
