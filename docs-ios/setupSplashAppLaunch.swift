4df491d89818fd9eff4016f179ec67c006576128
James Morris <47285129+jamesmorrisapps@users.noreply.github.com>
2021-06-18 06:25:40 -0700

    Removed Splash Delegates

--- docs-ios/setupSplashAppLaunch.swift
+++ docs-ios/setupSplashAppLaunch.swift
@@ -11,21 +11,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          SmaatoSDK.loadInterstitial(forAdSpaceId: "SMAATO_ADSPACE_ID", delegate: self, requestParams: params)
     }
 }
- 
-extension AppDelegate: SMAInterstitialDelegate {
-    // Interstitial successfully loaded
-    func interstitialDidLoad(_ interstitialResponse: SMAInterstitial) {
-        interstitial = interstitialResponse
-        interstitialResponse.show(from: self)
-    }
-     
-    // Interstitial failed to load
-    func interstitial(_ interstitial: SMAInterstitial?, didFailWithError error: Error) {
-       print("Interstitial did fail loading with error: \(error.localizedDescription)")
-    }
-     
-    // Interstitial ads TTL has expired    
-    func interstitialDidTTLExpire(_ interstitial: SMAInterstitial) {
-        print("Interstitial TTL has expired")
-    }
-}
