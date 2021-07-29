772980ed5220d31a8a2a8988736c5e1dfa19ffae
James Morris <47285129+jamesmorrisapps@users.noreply.github.com>
2021-07-29 02:04:00 -0700

    Added delegates

--- docs-ios/setupSplashSecondScreen.m
+++ docs-ios/setupSplashSecondScreen.m
@@ -15,5 +15,23 @@
     requestParams.isSplash = YES;
     [SmaatoSDK loadInterstitialForAdSpaceId:@"SMAATO_ADSPACE_ID" delegate:self requestParams:requestParams];
 }
+
+// Interstitial successfully loaded
+- (void)interstitialDidLoad:(SMAInterstitial *)interstitial {
+    self.interstitial = interstitial;
+    [self.interstitial showFromViewController:self];
+}
+  
+// Interstitial failed to load
+- (void)interstitial:(SMAInterstitial *)interstitial didFailWithError:(NSError *)error {
+    NSLog(@"Interstitial failed to load with error: %@", error.localizedDescription);
+}
+ 
+// Interstitial ads TTL has expired
+- (void)interstitialDidTTLExpire:(SMAInterstitial *)interstitial {
+    NSLog(@"Interstitial TTL has expired");
+}
+
+// all other interstitial delegates are supported
  
 @end
