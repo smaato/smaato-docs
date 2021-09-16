47cf54fd32b225eac45287e436d535c249564864
James Morris <47285129+jamesmorrisapps@users.noreply.github.com>
2021-09-16 13:01:26 -0700

    added correctly translated iOS code

--- docs-ios/usingNativeAds.swift
+++ docs-ios/usingNativeAds.swift
@@ -1,5 +1,5 @@
-internal var moPubNative: MoPubNative! = MoPubNative(self, NATIVE_AD_ID, self)
-var mediaViewBinder: SmaatoMoPubNativeViewBinder! = SmaatoMoPubNativeViewBinder
-var Builder: SmaatoMoPubNativeViewBinder!
-internal var mediaAdRenderer: SmaatoMoPubNativeRenderer! = SmaatoMoPubNativeRenderer(mediaViewBinder)
-moPubNative.registerAdRenderer(mediaAdRenderer)
+let settings = MPStaticNativeAdRendererSettings()
+settings.renderingViewClass = YourCustomNativeAdView.self
+     
+guard let config = MPStaticNativeAdRenderer.rendererConfiguration(with: settings) else { return }
+config.supportedCustomEvents = ["SMAMoPubSmaatoNativeAdapter"]
