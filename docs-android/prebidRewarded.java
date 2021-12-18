505be62a6ecb2bb8b9dd04360a2cd98520dcecb5
James Morris <47285129+jamesmorrisapps@users.noreply.github.com>
2021-12-17 16:46:08 -0800

    updated for GAM

--- docs-android/prebidRewarded.java
+++ docs-android/prebidRewarded.java
@@ -1,7 +1,8 @@
-//...
-UnifiedBidding.prebidRewardedInterstitial(SMAATO_UB_REWARDED_AD_SPACE_ID, prebidListener);
+private RewardedAd mRewardedAd;
+  private final String TAG = "MainActivity";
+
+UnifiedBidding.prebidRewardedInterstitial("SMAATO_ADSPACE_ID", prebidListener);
  
-//...
 private final UnifiedBidding.PrebidListener prebidListener = (ubBid, ubBidRequestError) ->
             Threads.runOnUi(() -> {
                 if (ubBidRequestError != null) {
@@ -18,11 +19,26 @@ private final UnifiedBidding.PrebidListener prebidListener = (ubBid, ubBidReques
                     SmaatoRewardedVideoMediationSettings.UNIQUE_ID_KEY,
                     ubBid.metadata.get(SmaatoRewardedVideoMediationSettings.UNIQUE_ID_KEY)
                 );
-                MoPubRewardedVideoManager.RequestParameters parameters =
-                    new MoPubRewardedVideoManager.RequestParameters(ubBid.mopubPrebidKeyword);
-                MoPubRewardedVideos.loadRewardedVideo(
-                    MOPUB_REWARDED_AD_UNIT_ID,
-                    parameters,
-                    settings
-                );
-            });
+             
+                AdManagerAdRequest kvpRequest = new AdManagerAdRequest.Builder().build();
+                Map<String, String> ubKVP = new HashMap<String, String>();
+                ubKVP.put("smaub", ubBid.mopubPrebidKeyword);
+                kvpRequest.customTargeting = ubKVP;
+             
+                
+                RewardedAd.load(this, "YOUR_GAM_AD_UNIT_ID", kvpRequest, new RewardedAdLoadCallback() {
+                  @Override
+                  public void onAdFailedToLoad(@NonNull LoadAdError loadAdError) {
+                  // Handle the error.
+                  Log.d(TAG, loadAdError.getMessage());
+                  mRewardedAd = null;
+                  }
+
+                 @Override
+                 public void onAdLoaded(@NonNull RewardedAd rewardedAd) {
+                 mRewardedAd = rewardedAd;
+                 Log.d(TAG, "Ad was loaded.");
+                 }
+               });
+             
+       });
