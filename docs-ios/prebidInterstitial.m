ac20789b1fd1e9df097236eebbbc0c201db62882
James Morris <47285129+jamesmorrisapps@users.noreply.github.com>
2021-12-17 16:08:33 -0800

    updated for GAM

--- docs-ios/prebidInterstitial.m
+++ docs-ios/prebidInterstitial.m
@@ -1,9 +1,8 @@
-MPInterstitialAdController *adInterstitial = [MPInterstitialAdController interstitialAdControllerForAdUnitId:@"MOPUB_ADUNIT_ID"];
-adInterstitial.delegate = self;
+@property(nonatomic, strong) GAMInterstitialAd *interstitial;
 [SmaatoSDK prebidInterstitialForAdSpaceId:@"SMAATO_ADSPACE_ID"
                                completion:^(SMAUbBid * _Nullable bid, NSError * _Nullable error) {
     if (bid) {
-        // Let's assume this is the max price
+        // Let's assume this is the max price of your line items (you will want to change this float to yours)
         CGFloat maxPrice = 0.1;
         NSString *bidKeyword;
                                             
@@ -12,23 +11,18 @@ adInterstitial.delegate = self;
         } else {
             bidKeyword = bid.mopubPrebidKeyword;
         }
-        NSString *adViewKeywords = adInterstitial.keywords;
-        if (adViewKeywords.length > 0) {
-            NSPredicate *predicate = [NSPredicate predicateWithFormat:@"NOT (SELF beginswith[c] 'smaato_cpm')"];
-            NSMutableArray *filteredKeywords = [[[adViewKeywords componentsSeparatedByString:@","] filteredArrayUsingPredicate:predicate] mutableCopy];
-            [filteredKeywords addObject:bidKeyword];
-            adInterstitial.keywords = [filteredKeywords componentsJoinedByString:@","];
-        } else {
-            adInterstitial.keywords = bidKeyword;
-        }
- 
-        if (adInterstitial.localExtras) {
-            NSMutableDictionary *extras = [NSMutableDictionary dictionaryWithDictionary:adInterstitial.localExtras];
-            [extras addEntriesFromDictionary:bid.metaData];
-            adInterstitial.localExtras = extras;
-        } else {
-            adInterstitial.localExtras = bid.metaData;
-        }
+        
+        GAMRequest kvpRequest = [GAMRequest request];
+        NSDictionary *ubKVP = @{@"smaub":bidKeyword};
+        kvpRequest.customTargeting = ubKVP;
     }
-    [adInterstitial loadAd];
+    [GAMInterstitialAd loadWithAdUnitID:@"YOUR_GAM_AD_UNIT_ID"
+                              request:kvpRequest
+                    completionHandler:^(GAMInterstitialAd *ad, NSError *error) {
+    if (error) {
+      NSLog(@"Failed to load interstitial ad with error: %@", [error localizedDescription]);
+      return;
+    }
+    self.interstitial = ad;
+    self.interstitial.fullScreenContentDelegate = self;
 }];
