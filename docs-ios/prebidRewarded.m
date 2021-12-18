edeb0b4c48c653ce2600e26f2807883b70ebea65
James Morris <47285129+jamesmorrisapps@users.noreply.github.com>
2021-12-17 16:33:00 -0800

    added comment

--- docs-ios/prebidRewarded.m
+++ docs-ios/prebidRewarded.m
@@ -13,7 +13,7 @@
         }
         
         GAMRequest kvpRequest = [GAMRequest request];
-        NSDictionary *ubKVP = @{@"smaub":bidKeyword};
+        NSDictionary *ubKVP = @{@"smaub":bidKeyword}; // make sure you add "smaub" as a Dynamic Key under Inventory >> Key-Values inside of GAM (no value as you will pass that here)
         kvpRequest.customTargeting = ubKVP;
     }
      [GADRewardedAd
