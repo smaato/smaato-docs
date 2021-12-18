e77d4f69b3a3eda3bda9c957cda40a38f9e9bbb0
James Morris <47285129+jamesmorrisapps@users.noreply.github.com>
2021-12-17 16:22:47 -0800

    updated for GAM

--- docs-ios/prebidRewarded.swift
+++ docs-ios/prebidRewarded.swift
@@ -1,9 +1,8 @@
-MPRewardedVideo.setDelegate(self, forAdUnitId: "MOPUB_ADUNIT_ID")
+var rewardedAd: GADRewardedAd?
 SmaatoSDK.prebidRewardedInterstitial(forAdSpaceId: "SMAATO_ADSPACE_ID") { (bid: SMAUbBid?, error: Error?) in
-    var keyword: String? = "SOME-OTHER-BID-KEYWORDS"
-    var bidExtra: [AnyHashable:Any]? = ["SOME-OTHER-KEY" : "SOME-OTHER-VALUE"]
+
     if let smaatoBid = bid {
-        // Let's assume this is the max price
+        // Let's assume this is the max price of your line items (you will want to change this float to yours)
         let maxPrice : CGFloat = 0.1
         let bidKeyword : String            
         if smaatoBid.bidPrice > maxPrice {
@@ -11,9 +10,21 @@ SmaatoSDK.prebidRewardedInterstitial(forAdSpaceId: "SMAATO_ADSPACE_ID") { (bid: 
         } else {
             bidKeyword = smaatoBid.mopubPrebidKeyword
         }
-        // or manual something like `let bidKeyword = "smaato" + "-" + "\(smaatoBid.bidPrice * 100)"`
-        keywords = (keywords != nil) ? keywords + ",\(bidKeyword)" : bidKeyword
-        bidExtra = (bidExtra != nil) ? bidExtra.merging(smaatoBid.metaData) { $1 } : smaatoBid.metaData
+        let kvpRequest = GAMRequest()
+        let ubKVP = [
+            "smaub": bidKeyword // make sure you add "smaub" as a Dynamic Key under Inventory >> Key-Values inside of GAM (no value as you will pass that here)
+        ]
+        kvpRequest.customTargeting = ubKVP
    }
-   MPRewardedVideo.loadAd(withAdUnitID: "MOPUB_ADUNIT_ID", keywords: keywords, userDataKeywords: nil, location: nil, customerId: nil, mediationSettings: [], localExtras: bidExtra)
+   GADRewardedAd.load(withAdUnitID:"YOUR_GAM_AD_UNIT_ID",
+                                request: kvpRequest,
+                      completionHandler: { [self] ad, error in
+                        if let error = error {
+                          print("Failed to load rewarded ad with error: \(error.localizedDescription)")
+                          return
+                        }
+                        rewardedAd = ad
+                        rewardedAd?.fullScreenContentDelegate = self
+                      }
+    )
 }
